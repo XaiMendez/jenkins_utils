65fbf70543920f28fce32db02ef809f29dc1faea
Xavier Mendez <xmendez@urbano.com.sv>
2021-10-04 13:45:30 -0600

    update

--- lumen/deploy/deploy.sh
+++ lumen/deploy/deploy.sh
@@ -4,5 +4,5 @@ echo "######################"
 echo "***     Deploy     ***"
 echo "######################"
 
-docker stack remove core_api_clients && \
-docker stack deploy core_api_clients --compose-file ../compose-files/simple-docker-compose.yml
+docker stack remove $SERVICE_NAME && \
+docker stack deploy $SERVICE_NAME --compose-file docker-compose.yml
